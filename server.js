0ea70862ffa5042a95fff089868caa09d02ab77b
joaolucas14 <jlparceiro10@gmail.com>
2025-02-12 16:11:19 -0300

    iniciando API

---
+++ server.js
@@ -0,0 +1,11 @@
+import http from "http";
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "content-type": "text/plain" });
+  res.end("Curso de Node.js");
+});
+
+server.listen(PORT, () => {
+  console.log("Servidor escutando");
+});
