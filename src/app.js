04de0300e52d9e685781762c5bb10866664b04c6
joaolucas14 <jlparceiro10@gmail.com>
2025-02-13 09:54:15 -0300

    adicionando configuração do banco de dados e modelo de livro

--- src/app.js
+++ src/app.js
@@ -1,31 +1,27 @@
 import express from "express";
+import conectaNaDataBase from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
+
+const conexao = await conectaNaDataBase();
+
+conexao.on("error", (erro) => {
+  console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+  console.log("Conexão realizada com sucesso");
+});
 
 const app = express();
 app.use(express.json());
-
-const livros = [
-  {
-    id: 1,
-    titulo: "O senhor dos aneis",
-  },
-  {
-    id: 2,
-    titulo: "O Hobbit",
-  },
-];
-
-function buscaLivro(id) {
-  return livros.findIndex((livro) => {
-    return livro.id === Number(id);
-  });
-}
 
 app.get("/", (req, res) => {
   res.status(200).send("Curso de Node.js");
 });
 
-app.get("/livros", (req, res) => {
-  res.status(200).json(livros);
+app.get("/livros", async (req, res) => {
+  const listaLivros = await livro.find({});
+  res.status(200).json(listaLivros);
 });
 
 app.get("/livros/:id", (req, res) => {
