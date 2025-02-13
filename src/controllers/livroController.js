f7aac4610e3659dd02055818b15d63ad354e2294
joaolucas14 <jlparceiro10@gmail.com>
2025-02-13 10:58:59 -0300

    implementando controlador e rotas para listar, cadastrar e atualizar livros

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -2,8 +2,25 @@ import livro from "../models/Livro.js";
 
 class LivroController {
   static async listarLivros(req, res) {
-    const listaLivros = await livro.find({});
-    res.status(200).json(listaLivros);
+    try {
+      const listaLivros = await livro.find({});
+      res.status(200).json(listaLivros);
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.message} - Falha na requisição` });
+    }
+  }
+  static async listarLivro(req, res) {
+    try {
+      const id = req.params.id;
+      const livroEncontrado = await livro.findById(id);
+      res.status(200).json(livroEncontrado);
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.message} - Falha na requisição do livro` });
+    }
   }
 
   static async cadastrarLivro(req, res) {
@@ -14,6 +31,17 @@ class LivroController {
       res
         .status(500)
         .json({ message: `${erro.message} - Falha ao cadastrar livro` });
+    }
+  }
+  static async atualizarLivro(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndUpdate(id, req.body);
+      res.status(200).json({ message: "livro atualizado" });
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.message} - Falha na atualização do livro` });
     }
   }
 }
