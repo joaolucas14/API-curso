0b31fc2363256b5474511fa05b301fdda8cd2ac1
joaolucas14 <jlparceiro10@gmail.com>
2025-02-20 09:07:02 -0300

    adicionando validações para o campo editora e ajustando limites para o número de páginas no modelo Livro

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -7,9 +7,23 @@ const livroSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "autores",
     required: [true, "O autor é obrigatório"],
+    enum: {
+      values: ["Casa do Código", "Alura"],
+      message: "A editora {VALUE} não é um valor permitido",
+    },
   },
   editora: { type: String, required: [true, "A editora é obrigatória"] },
-  numeroPaginas: { type: Number },
+  numeroPaginas: {
+    type: Number,
+    min: [
+      10,
+      "O número de página deve estar entre 10 e 5.000. Valor fornecido: {VALUE}",
+    ],
+    max: [
+      5000,
+      "O número de página deve estar entre 10 e 5.000. Valor fornecido: {VALUE}",
+    ],
+  },
 });
 
 const livros = mongoose.model("livros", livroSchema);
