70760c9cfa2e0485c43747a88ede9f09cbd28f22
joaolucas14 <jlparceiro10@gmail.com>
2025-02-20 10:56:43 -0300

    refatorando a função de busca para incluir filtros de páginas e tornar a busca por editora case-insensitive

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -79,12 +79,7 @@ class LivroController {
 
   static listarLivroPorFiltro = async (req, res, next) => {
     try {
-      const { editora, titulo } = req.query;
-
-      const busca = {};
-      if (editora) busca.editora = editora;
-      if (titulo) busca.titulo = { $regex: titulo, $options: "i" };
-
+      const busca = processaBusca(req.query);
       const livrosResultado = await livros.find(busca);
 
       res.status(200).send(livrosResultado);
@@ -92,6 +87,21 @@ class LivroController {
       next(erro);
     }
   };
+}
+
+function processaBusca(parametros) {
+  const { editora, titulo, minPaginas, maxPaginas } = parametros;
+
+  const busca = {};
+  if (editora) busca.editora = { $regex: editora, $options: "i" };
+  if (titulo) busca.titulo = { $regex: titulo, $options: "i" };
+
+  if (minPaginas || maxPaginas) busca.numeroPaginas = {};
+
+  if (minPaginas) busca.numeroPaginas.$gte = minPaginas;
+  if (maxPaginas) busca.numeroPaginas.$lte = maxPaginas;
+
+  return busca;
 }
 
 export default LivroController;
